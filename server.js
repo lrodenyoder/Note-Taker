5c3eee723316ff9a7f8f562bb7155568997f5a33
lrodenyoder <rodenlau000@gmail.com>
2022-03-08 18:31:37 -0600

    merging

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
 //move to lib/db.js 
